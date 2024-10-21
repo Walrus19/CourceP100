7d425d7205031636d10fc77fc41ed82679cbb87d
Walrus19 <dick19@yandex.ru>
2024-10-21 22:32:01 +0300

    2

--- Занятие 2. Условия и циклы/Лабораторные работы/Финансовая подушка безопасности/main.py
+++ Занятие 2. Условия и циклы/Лабораторные работы/Финансовая подушка безопасности/main.py
@@ -2,7 +2,7 @@ money_capital = 20000  # Подушка безопасности
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
-count = 1
+count = 2
 
 budget = money_capital + salary - spend
 # spend_add = spend * increase
