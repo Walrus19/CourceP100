5b9b243c553a4305e427c8aa7895562be776a1a9
Walrus19 <dick19@yandex.ru>
2024-11-01 21:35:30 +0300

    klass2

--- Занятие 4. Модули и работа с библиотеками/Самостоятельная работа/Усовершенствованая функция remove/main.py
+++ Занятие 4. Модули и работа с библиотеками/Самостоятельная работа/Усовершенствованая функция remove/main.py
@@ -1,10 +1,14 @@
 # TODO написать функцию remove
-def remove():
-    ...
+def remove(list_, value):
+    if not value in list_:
+        raise ValueError()
+    index = list_.index(value)
+    return list_[:index] + list_[index+1:]
 
 
-# print(remove([0, 1, 2, 0, 1, 2], 0))  # [0, 1, 2, 1, 2]
-# print(remove([0, 1, 2], 0))  # [1, 2]
-# print(remove([0, 1, 2, 3, 4], 4))  # [0, 1, 2, 3]
+print(remove([0, 1, 2, 0, 1, 2], 0))  # [0, 1, 2, 1, 2]
+print(remove([0, 1, 2], 0))  # [1, 2]
+print(remove([0, 1, 2, 3, 4], 4))  # [0, 1, 2, 3]
 
-print(list(range(110)))
+list__ = [0 , 1 , 2, 3, 7, 5]
+# print(remove(list__, 7))
