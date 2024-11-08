1e3a5dd000814a49b20ce40d974fedc80ec005e6
Walrus19 <dick19@yandex.ru>
2024-11-08 16:58:08 +0300

    5

--- Занятие 5. Работа с источниками данных/Практические задания/Запись лесенки в файл/main.py
+++ Занятие 5. Работа с источниками данных/Практические задания/Запись лесенки в файл/main.py
@@ -1,16 +1,23 @@
+from idlelib.iomenu import encoding
+
 INPUT_FILE = "input.txt"
 
 
 OUTPUT_FILE = "output.txt"
 
 
-def task():
+def task(count):
     ...  # TODO построчно записать лесенку в файл
+    with open(OUTPUT_FILE, "w", encoding="utf-8") as f1:
+        for i in range(count):
+            str = "*" * i+"\n"
+            f1.write(str)
+
 
 
 if __name__ == '__main__':
     # Необходимо для проверки
-    task()
+    task(10)
     with open(OUTPUT_FILE, encoding="utf-8") as file:
         for line in file:
             print(line, end="")
